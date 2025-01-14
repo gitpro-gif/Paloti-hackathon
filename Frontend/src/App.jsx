8658f84367208c08d65a07121ac43b6af8154722
gitpro-gif <vedantgithub2@gmail.com>
2025-01-15 01:06:20 +0530

    first commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Landing from './components/Landing'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' element = {<Landing />} />
+        </Routes>
+      </Router>
+    </>
+  )
+}
+
+export default App
